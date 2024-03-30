23e292a06304894d7d33c5c71bd1108eb9cb3680
amir soltani <amirsoltani686@gmail.com>
2024-03-30 17:00:43 +0330

    Initial Commit

---
+++ NetworkSample/NetworkSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkSampleApp.swift
+//  NetworkSample
+//
+//  Created by Amir on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
